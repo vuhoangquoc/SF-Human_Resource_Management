5490a09cb4c3c5b809dced1301068145c6e1c3c8
vuhoangquoc <hoangvu280701@gmail.com>
2023-04-14 10:34:16 +0700

    Update Department Page - save in localstorage, conect with Personal Page

--- src/dummyDate.js
+++ src/dummyDate.js
@@ -1,93 +1,26 @@
 export const PartitiveData = [
-    {
-        id:1,
-        departmentId: "FE0145",
-        departmentName: "Phòng IT",
-        status:""
-
-    },
-    {
-        id:2,
-        departmentId: "SF324",
-        departmentName: "Phòng Magketin",
-        status:""
-
-    },
-    {
-        id:3,
-        departmentId: "SS234",
-        departmentName: "Servike",
-        status:""
-
-    },
-    {
-        id:4,
-        departmentId: "DG123",
-        departmentName: "Rom 1",
-        status:""
-
-    },
-    {
-        id:5,
-        departmentId: "DG2",
-        departmentName: "Rom 2",
-        status:""
-
-    },
-    {
-        id:6,
-        departmentId: "DG1",
-        departmentName: "Rom 3",
-        status:""
-
-    },
-    {
-        id:7,
-        departmentId: "DG9",
-        departmentName: "Rom 4",
-        status:""
-
-    },
-    {
-        id:8,
-        departmentId: "DG9",
-        departmentName: "Rom 4",
-        status:""
-
-    },
-    {
-        id:9,
-        departmentId: "DG9",
-        departmentName: "Rom 4",
-        status:""
-
-    },
-    {
-        id:10,
-        departmentId: "DG9",
-        departmentName: "Rom 4",
-        status:""
-
-    },
-    {
-        id:11,
-        departmentId: "DG9",
-        departmentName: "Rom 4",
-        status:""
-
-    },
-    {
-        id:12,
-        departmentId: "DG9",
-        departmentName: "Rom 4",
-        status:""
-
-    },
-    {
-        id:13,
-        departmentId: "DG9",
-        departmentName: "Rom 4",
-        status:""
-
-    },
-]
+  {
+    id: 1,
+    departmentId: "FE0145",
+    departmentName: "IT",
+    status: "",
+  },
+  {
+    id: 2,
+    departmentId: "SF324",
+    departmentName: "Services",
+    status: "",
+  },
+  {
+    id: 3,
+    departmentId: "SS234",
+    departmentName: "Support",
+    status: "",
+  },
+  {
+    id: 4,
+    departmentId: "DG123",
+    departmentName: "Markerting",
+    status: "",
+  },
+];
